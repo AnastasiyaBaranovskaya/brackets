648a8eea03677b4a84946f3376bf6b8531b1e54e
Anastasiya <anastasiya.baranouskaya@gmail.com>
2019-02-23 11:45:42 +0300

    nine passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let stack = 0;
+  for (let i = 0; i < bracketsConfig.length; i++){
+    if (i == '[' || i == '(' || i == '{') {
+      stack += 1;
+    };
+    if (i == ']' || i == ')' || i == '}'){ 
+      stack -= 1;
+    };
+  };
+  if (stack == 0 ){
+    return check == true;
+  }
+  else {
+    return check == false;
+  }
+};

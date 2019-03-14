d78234cf83fc052345ebb026fef87ac123496958
Anastasiya <anastasiya.baranouskaya@gmail.com>
2019-03-14 15:40:11 +0300

    brackets 13 passing
    Signed-off-by: Anastasiya <anastasiya.baranouskaya@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  var len = str.length; 
-  var open = 0; 
-  var close = 0; 
- 
-  for (var i = 0; i < len; i++) { 
-    if ( str[i] == '(' || '{' || '[') {
-    open += 1; 
+  let len = str.length; 
+  let brackets = 0;  
+  for (let i = 0; i <= len; i++) { 
+    for (let i = 0; i <= len; i++) { 
+    if ( (str[i] == ')') && (str[i-1] == '(')) {
+      brackets += 1; 
+      str = str.slice(0, i-1) + str.slice(i+1, len);
     }
   }
-  for (var i = len-1; i >= 0; i--) 
-  { 
-    if (str[i] == ')' || ']' || '}' ){
-      close += 1; 
-    }
+  for (let i = 0; i <= len; i++) { 
+  if ( (str[i] == '}') && (str[i-1] == '{')) {
+    brackets += 1; 
+    str = str.slice(0, i-1) + str.slice(i+1, len);
+  }
+}
+  for (let i = 0; i <= len; i++) { 
+  if ( (str[i] == ']') && (str[i-1] == '[')) {
+    brackets += 1; 
+    str = str.slice(0, i-1) + str.slice(i+1, len);
+  }
+}
+}
+  if (brackets == len/2 ){
+    return true;
   } 
-  return open === close;
+  else {
+    return false;
+  }
 };
